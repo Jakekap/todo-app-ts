4d5a4ace94126b337b5c4d21a569c6c39f520e5a
Christian Andrade <ce.andrade.p@gmail.com>
2023-06-23 08:54:49 -0500

    :sparkles: Add check function
    add check and delete

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,22 +1,32 @@
-import { type Todo as TodoType } from '../types'
+import { type TodoId, type Todo as TodoType } from '../types'
 import { useState } from 'react'
 
 interface Props extends TodoType {
-  onRemoveTodo: (id: string) => void
+  onRemoveTodo: ({ id }: TodoId) => void
+  onToggleCompleted: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
 }
 
-export const Todo: React.FC<Props> = ({ id, completed, onRemoveTodo }) => {
+export const Todo: React.FC<Props> = ({ id, completed, onRemoveTodo, onToggleCompleted }) => {
   const [text, setText] = useState('')
 
   return (
         <div className="view">
-            <input className="toggle" type="checkbox" checked={completed} onChange={() => {}} />
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={completed}
+              onChange={
+                (event) => {
+                  onToggleCompleted({ id, completed: event.target.checked })
+                }
+              }
+            />
             <input className="" type="text" value={text} onChange={e => { setText(e.target.value) }} />
             <label>{text}</label>
             <button
                 className="destroy"
                 onClick={() => {
-                  onRemoveTodo(id)
+                  onRemoveTodo({ id })
                 }}
             />
         </div>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,13 @@
-import { type ListOfTodos } from '../types'
+import { type TodoId, type ListOfTodos, type Todo as TodoType } from '../types'
 import { Todo } from './Todo'
 
 interface Props {
   todos: ListOfTodos
-  onRemoveTodo: (id: string) => void
+  onRemoveTodo: ({ id }: TodoId) => void
+  onToggleCompleted: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
 }
 
-export const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
+export const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onToggleCompleted }) => {
   return (
         <ul className='todo-list'>
             {todos.map(todo => (
@@ -18,6 +19,7 @@ export const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
                   id={todo.id}
                   completed={todo.completed}
                   onRemoveTodo={onRemoveTodo}
+                  onToggleCompleted={onToggleCompleted}
                   />
                 </li>
             ))}
